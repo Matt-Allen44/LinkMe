2825a5e92114333d651948457c11afaf65cc71b2
Matthew Allen <whosdaz@gmail.com>
2016-09-16 19:19:06 +0930

    Added first implementation using Hash tables - will be migrated to redis later

--- LinkMe.js
+++ LinkMe.js
@@ -5,34 +5,44 @@ var linkme = express();
 //Default starter appended to the link eg. http://www.link.me/{HashHere}
 var LINKME_BASE_URL = 'localhost'
 
+var shortURLs = new Object(); // or var map = {};
+
+
 var ShortenLink = function(url){
   //Hash function using md5 and return chars 0-10 for a shorter link
   //Definant potential for hash collision though -- LIMITATION SHOULD BE DEALT WITH
-  return crypto.createHash('md5').update(url).digest('hex').substr(0,10);
+  return crypto.createHash('md5').update(url).digest('hex').substr(0,6);
 };
 
 var sendToRedis = function(shortURL, longURL){
-
+  shortURLs[shortURL] = longURL;
 };
 
-var fetchFromRedis = function(shortURL, longURL){
-
+var fetchFromRedis = function(shortURL){
+  console.log('FETCHED ' + shortURL + " --> " + shortURLs[shortURL])
+  return shortURLs[shortURL];
 };
 
 linkme.get('/shorten', function(req, res){
   res.status(200);
-  res.end(LINKME_BASE_URL + '/' + ShortenLink(req.query.url));
+  console.log('Shortened ' + req.query.url + " to /" + ShortenLink(req.query.url));
+  sendToRedis('/' + ShortenLink(req.query.url), req.query.url);
+  res.sendFile(__dirname  + '/showurl.html');
+});
+
+
+linkme.get('/', function(req, res){
+  res.status(200);
+  res.sendFile(__dirname  + '/linkme.html');
 });
 
 linkme.get('*', function(req, res){
-  res.status(200);
-  res.write('OK: ' + req.originalUrl);
-  res.end('Redirect to: ' )
+  res.writeHead(301,
+  {Location: shortURLs[req.originalUrl]}
+);
+  res.end();
 });
 
 linkme.listen(80, function(){
   console.log('Listening on :80')
 });
-console.log(ShortenLink('www.google.com'));
-console.log(ShortenLink('www.reddit.com'));
-console.log(ShortenLink('www.reddit.com/r/cscareerquestions/comments/52jcx4/resume_advice_thread_september_13_2016/'));
